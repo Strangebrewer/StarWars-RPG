78b4b8859ad4bd5e7f407ea7ab314ad4ed1cbac0
Strangebrewer <BKAShambala@gmail.com>
2018-02-06 01:57:28 -0700

    initial file structure

---
+++ assets/js/game.js
@@ -0,0 +1,56 @@
+var arePeeGeeGame = {
+
+playerOne: {
+  name: ""
+  , healthPoints: 250
+  , attackPower: 18
+  , counterAttackPower: 22
+}
+
+, playerTwo: {
+  name: ""
+  , healthPoints: 200
+  , attackPower: 14
+  , counterAttackPower: 27
+}
+
+, playerThree: {
+  name: ""
+  , healthPoints: 180
+  , attackPower: 22
+  , counterAttackPower: 16
+}
+
+, playerFour: {
+  name: ""
+  , healthPoints: 140
+  , attackPower: 16
+  , counterAttackPower: 30
+}
+
+
+
+
+
+
+
+
+
+// POSSIBLE CODE FOR A GAME WHERE YOU PICK THE THEME
+
+// , pickTheme: function() {
+//   // click button
+
+// }
+
+
+} // curly brace to close the RPG Object
+
+// buttons - one for each theme
+// once you click the button, the theme lays out and the buttons disappear
+
+
+// possible themes:
+  // so-shall media; names - Rebbit, Facebake, Instagroan, Twatter
+  // Battle of the Bands
+  //
