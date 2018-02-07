478983537682e333990fb4925e26dcd114e01667
Strangebrewer <BKAShambala@gmail.com>
2018-02-07 09:17:27 -0700

    basic game done - needs fine tuning

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,7 +12,6 @@ var playerOne;
 var playerTwo;
 
 // Functions
-
 function chooseAvatarFadeout() {
   $(".main-container").css("animation", "fadeout 1s");
   $(".main-container").css("opacity", "0");
@@ -28,6 +27,8 @@ function avatarFadein() {
   $("#choose-enemy-two").css("animation", "fadein2 2s");
   $("#choose-enemy-three").css("animation", "fadein2 2s");
   $("#your-enemies-label").css("animation", "fadein 2s");
+  $("#your-enemies-label").css("animation", "opac .8s");
+  $("#your-enemies-label").css("animation-iteration-count", "infinite");
   $(".choose-avatar").attr("disabled", true);
 }
 
@@ -86,13 +87,13 @@ function enemyAvatar(p1, name, imageLocation, healthId, healthPoints) {
   $(".choose-enemy img").css("border", "6px solid #9b0000");
 }
 
-function cloneEnemyAvatar(p1, p2) {
+function cloneEnemyAvatar(p1, p2, p3) {
   $(p1).clone()
     .css("padding", "10px 30px")
     .css("animation", "fadein 2s")
     .css("opacity", "1")
     .attr("class", "chosen-enemy")
-    .attr("id", "defender-one")
+    .attr("id", p3)
     .appendTo("#defender-avatar");
   $("#attack-button").css("display", "unset");
   $("#attack-button").css("animation", "fadein 2s");
@@ -100,8 +101,10 @@ function cloneEnemyAvatar(p1, p2) {
   $("#versus").css("animation", "fadein 2s");
   $(p2).css("display", "none");
   $(".choose-enemy").attr("disabled", true);
+  $("#attack-button").attr("disabled", false);
   $("#defender-avatar-label").text("Defender");
   $("#defender-avatar-label").css("animation", "fadein 2s");
+  $("#your-enemies-label").css("animation", "unset");
 }
 
 // Sets the enemy avatars after the Player Avatar has been chosen
@@ -114,96 +117,123 @@ function chooseAvatarFunction() {
   avatarFadein();
 }
 
-// Chooses which enemy avatar you wish to fight
+// BUTTONS TO CHOOSE YOUR DEFENDER
 $("#enemy-one").on("click", "#choose-enemy-one", function () {
-  cloneEnemyAvatar("#choose-enemy-one", "#enemy-one");
+  cloneEnemyAvatar("#choose-enemy-one", "#enemy-one", enemyOne.playerId);
   playerTwo = enemyOne;
+  currentTwoHealth = enemyOne.healthPoints;
 });
 
-$("#enemy-two").on("click","#choose-enemy-two", function () {
-  cloneEnemyAvatar("#choose-enemy-two", "#enemy-two");
+$("#enemy-two").on("click", "#choose-enemy-two", function () {
+  cloneEnemyAvatar("#choose-enemy-two", "#enemy-two", enemyTwo.playerId);
   playerTwo = enemyTwo;
+  currentTwoHealth = enemyTwo.healthPoints;
 });
 
 $("#enemy-three").on("click", "#choose-enemy-three", function () {
-  cloneEnemyAvatar("#choose-enemy-three", "#enemy-three");
+  cloneEnemyAvatar("#choose-enemy-three", "#enemy-three", enemyThree.playerId);
   playerTwo = enemyThree;
+  currentTwoHealth = enemyThree.healthPoints;
 });
 
-// Choose your avatar and populating the enemies list
+// BUTTONS TO CHOOSE YOUR AVATAR
 $("#choose-player-one").on("click", function () {
   chooseAvatarFunction();
   yourAvatar(vader.name, vader.imageLocation, vader.healthId, vader.healthPoints);
   playerOne = vader;
+  currentOneHealth = playerOne.healthPoints;
   enemyOne = obiwan;
   enemyTwo = luke;
   enemyThree = porg;
   enemyAvatar("#choose-enemy-one", obiwan.name, obiwan.imageLocation, obiwan.healthId, obiwan.healthPoints);
   enemyAvatar("#choose-enemy-two", luke.name, luke.imageLocation, luke.healthId, luke.healthPoints);
   enemyAvatar("#choose-enemy-three", porg.name, porg.imageLocation, porg.healthId, porg.healthPoints);
-  console.log(playerOne.name, enemyOne.name, enemyTwo.name, enemyThree.name);
 });
 
 $("#choose-player-two").on("click", function () {
   chooseAvatarFunction();
   yourAvatar(obiwan.name, obiwan.imageLocation, obiwan.healthId, obiwan.healthPoints);
   playerOne = obiwan;
+  currentOneHealth = playerOne.healthPoints;
   enemyOne = vader;
   enemyTwo = luke;
   enemyThree = porg;
   enemyAvatar("#choose-enemy-one", vader.name, vader.imageLocation, vader.healthId, vader.healthPoints);
   enemyAvatar("#choose-enemy-two", luke.name, luke.imageLocation, luke.healthId, luke.healthPoints);
   enemyAvatar("#choose-enemy-three", porg.name, porg.imageLocation, porg.healthId, porg.healthPoints);
-  console.log(playerOne.name, enemyOne.name, enemyTwo.name, enemyThree.name);
 });
 
 $("#choose-player-three").on("click", function () {
   chooseAvatarFunction();
   yourAvatar(luke.name, luke.imageLocation, luke.healthId, luke.healthPoints);
   playerOne = luke;
+  currentOneHealth = playerOne.healthPoints;
   enemyOne = vader;
   enemyTwo = obiwan;
   enemyThree = porg;
   enemyAvatar("#choose-enemy-one", vader.name, vader.imageLocation, vader.healthId, vader.healthPoints);
   enemyAvatar("#choose-enemy-two", obiwan.name, obiwan.imageLocation, obiwan.healthId, obiwan.healthPoints);
   enemyAvatar("#choose-enemy-three", porg.name, porg.imageLocation, porg.healthId, porg.healthPoints);
-  console.log(playerOne.name, enemyOne.name, enemyTwo.name, enemyThree.name);
 });
 
 $("#choose-player-four").on("click", function () {
   chooseAvatarFunction();
   yourAvatar(porg.name, porg.imageLocation, porg.healthId, porg.healthPoints);
   playerOne = porg;
+  currentOneHealth = playerOne.healthPoints;
   enemyOne = vader;
   enemyTwo = obiwan;
   enemyThree = luke;
   enemyAvatar("#choose-enemy-one", vader.name, vader.imageLocation, vader.healthId, vader.healthPoints);
   enemyAvatar("#choose-enemy-two", obiwan.name, obiwan.imageLocation, obiwan.healthId, obiwan.healthPoints);
   enemyAvatar("#choose-enemy-three", luke.name, luke.imageLocation, luke.healthId, luke.healthPoints);
-  console.log(playerOne.name, enemyOne.name, enemyTwo.name, enemyThree.name);
 });
 
-$("#attack-button").on("click", function() {
-  var attacks = 0;
-  var currentOneHealth = playerOne.healthPoints;
-  var currentTwoHealth = playerTwo.healthPoints;
-  var currentOneAttackPower = playerOne.attackPower;
-  var currentTwoAttackPower = playerTwo.attackPower;
+var undefeatedEnemies = 3
+var attacks = 0;
+var currentOneHealth;
+var currentTwoHealth;
 
-  playerTwo.healthPoints -= playerOne.attackPower;
-  playerOne.healthPoints -= playerTwo.counterAttackPower;
+// ATTACK BUTTON
+$("#attack-button").on("click", function () {
 
-  // so, in addition to reducing each one's health points, I also need to:
-      // increase attack power with each attack
-      // run a conditional to check when health points hit zero, followed by...
-      // prompt the user to choose the next defender (or tell them they lost because they're dead...)
-      // all while constantly pushing the results to the screen
+  if (attacks > 0) {
+    currentOneAttackPower += playerOne.attackPower;
+  } else {
+    currentOneAttackPower = playerOne.attackPower;
+  }
+  currentTwoHealth -= currentOneAttackPower;
+  currentOneHealth -= playerTwo.counterAttackPower;
 
+  $("#" + playerOne.healthId).text("Health: " + currentOneHealth);
+  $("#" + playerTwo.healthId).text("Health: " + currentTwoHealth);
 
-  console.log(playerOne.healthPoints, playerTwo.healthPoints);
+  if (currentTwoHealth <= 0) {
+    $("#attack-button").attr("disabled", true);
+    $("#your-enemies-label").text("Choose Next Defender");
+    $("#your-enemies-label").css("animation", "opac .8s");
+    $("#your-enemies-label").css("animation-iteration-count", "infinite");
+    $(".choose-enemy").attr("disabled", false);
+    $("#" + playerTwo.playerId).css("display", "none");
+    undefeatedEnemies--;
+    if (undefeatedEnemies === 0) {
+      $("#your-enemies-label").css("display", "none");
+      $("#attack-button").css("display", "none");
+      $("#defender-avatar-label").css("display", "none");
+      $("#versus").css("display", "none");
+    }
+  }
+
+  if (currentOneHealth <= 0) {
+    alert("You lose!");
+    $("#attack-button").attr("disabled", true);
+  }
+
   attacks++;
 });
 
+// prompt the user to choose the next defender (or tell them they lost because they're dead...)
+// all while constantly pushing the results to the screen
 
 // Character Objects
 var vader = {
@@ -213,6 +243,7 @@ var vader = {
   , counterAttackPower: 22
   , imageLocation: "assets/images/vader.jpg"
   , healthId: "vader-health"
+  , playerId: "vader-avatar"
 }
 
 var obiwan = {
@@ -222,6 +253,7 @@ var obiwan = {
   , counterAttackPower: 27
   , imageLocation: "assets/images/obi-wan.jpg"
   , healthId: "obiwan-health"
+  , playerId: "obiwan-avatar"
 }
 
 var luke = {
@@ -231,6 +263,7 @@ var luke = {
   , counterAttackPower: 16
   , imageLocation: "assets/images/luke.jpg"
   , healthId: "luke-health"
+  , playerId: "luke-avatar"
 }
 
 var porg = {
@@ -240,6 +273,7 @@ var porg = {
   , counterAttackPower: 30
   , imageLocation: "assets/images/porg.jpg"
   , healthId: "porg-health"
+  , playerId: "porg-avatar"
 }
 
 
